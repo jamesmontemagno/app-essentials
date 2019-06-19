d4966fae2821d6c2e10d15d5db626ce60166dbc4
James Montemagno <james.montemagno@gmail.com>
2019-06-19 13:41:30 -0700

    Add sms/phonedialer samples

---
+++ AppEssentials.Shared/Pages/PhoneDialerPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppEssentials.Shared.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PhoneDialerPage : ContentPage
+    {
+        public PhoneDialerPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                PhoneDialer.Open(EntryNumber.Text);
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Unable to make call", "Please enter a number", "OK");
+            }
+        }
+    }
+}
---
+++ AppEssentials.Shared/Pages/SmsPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppEssentials.Shared.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SmsPage : ContentPage
+    {
+        public SmsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                await Sms.ComposeAsync(new SmsMessage(EntryMessage.Text,
+                    EntryNumber.Text));
+
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Unable to send sms", "Check phone number and try again.", "OK");
+            }
+        }
+    }
+}
