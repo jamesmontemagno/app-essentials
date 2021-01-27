14e59b7869718b1c28ed7bd1f75687fc3ee7cd3d
James Montemagno <james.montemagno@gmail.com>
2021-01-27 13:02:40 -0800

    Add screenshot and share file apis

---
+++ AppEssentials.Shared/Pages/ScreenshotPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppEssentials.Shared.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScreenshotPage : ContentPage
+    {
+        public ScreenshotPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (Screenshot.IsCaptureSupported)
+            {
+                var screenshot = await Screenshot.CaptureAsync();
+                var stream = await screenshot.OpenReadAsync();
+
+                ImageScreenshot.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+    }
+}
--- AppEssentials.Shared/Pages/SharePage.xaml.cs
+++ AppEssentials.Shared/Pages/SharePage.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Essentials;
+using System.IO;
 
 namespace AppEssentials.Shared.Pages
 {
@@ -20,10 +21,30 @@ namespace AppEssentials.Shared.Pages
 
         private async void ButtonShare_Clicked(object sender, EventArgs e)
         {
+            var info = EntryShare.Text;
+            if (string.IsNullOrWhiteSpace(info))
+                return;
             await Share.RequestAsync(new ShareTextRequest
             {
-                Text = EntryShare.Text,
+                Text = info,
                 Title = "Share!"
+            });
+        }
+
+        private async void ButtonShareFile_Clicked(object sender, EventArgs e)
+        {
+            var info = EntryShare.Text;
+            if (string.IsNullOrWhiteSpace(info))
+                return;
+
+            var path = string.Empty;
+            path = Path.Combine(FileSystem.CacheDirectory, "Attachment.txt");
+            File.WriteAllText(path, info);
+
+            await Share.RequestAsync(new ShareFileRequest
+            {
+                Title = "Hello from Xamarin.Essentials",
+                File = new ShareFile(path)
             });
         }
     }
